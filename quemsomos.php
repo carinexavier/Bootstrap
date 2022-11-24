360e2b24b50b38f5ec53b9838dd543146f12effa
carine xavier <carine.xavier.x.x@gmail.com>
2022-11-23 21:40:14 -0300

    23/11

---
+++ quemsomos.php
@@ -0,0 +1,29 @@
+ <?php
+    require_once 'head.php';
+    require_once 'menu.php';
+ ?>
+
+ <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+        </div>
+        <div class="col-md-6"> 
+            <img src="imagens/relampagomarquinho.jpg">
+        </div>
+        <div class="col-md-6"> 
+            <h3>Quem Somos</h3>
+            <p>vhjbdbcdkl</p>
+        </div>
+        <div class="col-md-6"> 
+            <h3>Nossos Valores</h3>
+            <p>vhjbdbcdkl</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+        </div>
+    </div>
+</div>
+
+<?php
+?>
